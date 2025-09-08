42b6fdb54197e3a9875c6b169bd510fb4b802fe9
mdsharifulislam-r <masom3456@gmail.com>
2025-09-08 13:00:36 +0600

    fixs

--- api/index.js
+++ api/index.js
@@ -12,7 +12,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:["https://sahdat-vai-work.vercel.app"],
+  credentials:true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
